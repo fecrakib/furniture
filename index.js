fdfefd411a80477798de9309198cdd3772d5e07c
fecrakib <rakibislam132575@gmail.com>
2024-04-23 15:08:45 +0600

    calculator  completed

--- index.js
+++ index.js
@@ -1,55 +1,19 @@
-let balance = 0;
-const deposit = () => {
-    let accountName = document.getElementById('account').value;
-    const depositInput = document.getElementById('depositAmount');
+function appendDisply(value) {
+document.getElementById('display').value +=value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function calculate() {
+const exp =document.getElementById('display').value  ;
+try {
+     const result = eval(exp);
+     document.getElementById('display').value =result;
+} catch (error) {
+document.getElementById('display').value='Error';
     
-    let depositAmount = parseFloat(depositInput.value);
-    if(isNaN(depositAmount)||depositAmount<=0){
-        alert("Please enter a valid amount");
-        return;
-    }else{
-        balance +=depositAmount;
-        updateBalance();
-      
-        alert(`Deposit of $${depositAmount} successfully added`);
-       depositInput.value="";
-        return;
-    
-      
- 
-    }
-
-
-};
-
-
-const withdraw = () => {
-    let accountName = document.getElementById('account').value;
-    const withdrawInput = document.getElementById('withdrawAmount');
-    const withdrawAmount = parseFloat(withdrawInput.value);
-    console.log(withdrawAmount)
-    if(isNaN(withdrawAmount)||withdrawAmount<=0){
-        alert("Please enter a valid amount");
-        return;
-    }
-   
-    if(withdrawAmount > balance)
-        {
-            alert('Insufficient funds.');
-            return;
-
-        }else{
-            balance -= withdrawAmount;
-            updateBalance();
-            alert(`Withdrawal of $${withdrawAmount} successful. New balance: $${balance}`);
-            withdrawInput.value = ''; // Clear the input after the operation
-        }
-    }
-
-
-    const updateBalance = () => {
-        document.getElementById('balance').value=balance.toFixed(2);
-    };
-
-document.getElementById('depositButton').addEventListener('click', deposit);
-document.getElementById('withdrawButton').addEventListener('click',withdraw);
+}
+  }
+  
