02f3082810a40276cf66e6869e87b3ee0f1336c2
fecrakib <rakibislam132575@gmail.com>
2024-04-23 12:16:27 +0600

    withdraw section  addd

--- index.js
+++ index.js
@@ -2,39 +2,54 @@ let balance = 0;
 const deposit = () => {
     let accountName = document.getElementById('account').value;
     const depositInput = document.getElementById('depositAmount');
-    let depositAmount = parseFloat(document.getElementById('depositAmount').value);
+    
+    let depositAmount = parseFloat(depositInput.value);
     if(isNaN(depositAmount)||depositAmount<=0){
         alert("Please enter a valid amount");
         return;
     }else{
         balance +=depositAmount;
         updateBalance();
+      
         alert(`Deposit of $${depositAmount} successfully added`);
-        depositInput.value="";
+       depositInput.value="";
+        return;
+    
+      
  
     }
 
 
 };
+
+
 const withdraw = () => {
     let accountName = document.getElementById('account').value;
-    let withdrawAmount = parseFloat(document.getElementById('withdrawAmount').value);
+    const withdrawInput = document.getElementById('withdrawAmount');
+    const withdrawAmount = parseFloat(withdrawInput.value);
     console.log(withdrawAmount)
     if(isNaN(withdrawAmount)||withdrawAmount<=0){
         alert("Please enter a valid amount");
         return;
-    }else if(balance!==0)
+    }
+   
+    if(withdrawAmount > balance)
         {
-            balance -=withdrawAmount;
-            updateBalance();
-            alert(`$${withdraw} has completed.Now is ${balance}`);
+            alert('Insufficient funds.');
             return;
 
+        }else{
+            balance -= withdrawAmount;
+            updateBalance();
+            alert(`Withdrawal of $${withdrawAmount} successful. New balance: $${balance}`);
+            withdrawInput.value = ''; // Clear the input after the operation
         }
     }
 
-const updateBalance = () => {
-    document.getElementById('balance').value=balance.toFixed(2);
-};
+
+    const updateBalance = () => {
+        document.getElementById('balance').value=balance.toFixed(2);
+    };
+
 document.getElementById('depositButton').addEventListener('click', deposit);
 document.getElementById('withdrawButton').addEventListener('click',withdraw);
